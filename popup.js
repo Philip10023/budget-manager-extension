917f18b719a9a588a897d245aae108cbd39b2126
Philip10023 <philipormorgan@gmail.com>
2018-08-12 04:28:17 -0400

    added context menu, highlighting can add money to total

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ $(function(){
     chrome.storage.sync.get(['total', 'limit'], function(budget){
       $('#total').text(budget.total);
       $('#limit').text(budget.limit)
-    })
+    });
 
     $('#spendAmount').click(function(){
 
@@ -18,12 +18,14 @@ $(function(){
               newTotal += parseInt(amount);
 
             }
+
             chrome.storage.sync.set({'total': newTotal}, function(){
-              if(amount && newTotal >= budget.limit){
+              if (amount && newTotal >= budget.limit){
                 var notifOptions = {
                   type: 'basic',
                   iconUrl: 'icon48.png',
                   title: 'Limit reached!',
+                  priority: 1,
                   message: "You've reached your limit!"
                 };
                 chrome.notifications.create('limitNotif', notifOptions);
